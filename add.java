08931426906065cc4ae05f18be512eb7f1b9cdb4
Snehabajantri25 <snehabajantri051@gmail.com>
2025-04-09 09:01:39 +0530

    updated

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add {
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
+        int a=100;
+        int b=200;
         int sum=a+b;
         System.out.println("The sum is" +sum);
     }
